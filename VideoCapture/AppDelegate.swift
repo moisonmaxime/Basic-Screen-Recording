4a737f1242bb92a227f4e2fe6e364666fd17a51b
Maxime Moison <Maxime@Maximes-MacBook-Pro.local>
2017-08-23 14:13:54 -0700

    Initial Commit

---
+++ VideoCapture/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  VideoCapture
+//
+//  Created by Maxime Moison on 8/23/17.
+//  Copyright © 2017 Maxime Moison. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ VideoCapture/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  VideoCapture
+//
+//  Created by Maxime Moison on 8/23/17.
+//  Copyright © 2017 Maxime Moison. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
